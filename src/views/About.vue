31eea283b9777a6198f02ede54378de1a263dc7a
sauloborghi <30803762+sauloborghi@users.noreply.github.com>
2021-06-16 20:02:41 -0300

    Add files via upload

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,19 +1,10 @@
 <template>
   <div id="app">
-    <br>
-    <h3 class="center">Ultimas Noticias</h3>
-    <h1 class="center">Hora de ficar atualizado</h1>
-    <hr class="hr">
-  <b-card-group deck>
-    <b-card title="Taquaritinga já tem mais de 10% da população vacinada contra COVID-19" img-src="https://www.cornellsci.com/images/about-001.jpeg" img-alt="Image" img-top>
-      <b-card-text>
-        Mesmo com a grande dificuldade na obtenção de vacinas contra a COVID-19 neste momento que estamos vivendo, com o início da vacinação de idosos com 70 anos ou mais nesta terça-feira (23), Taquaritinga alcançou a marca de 10,17% de pessoas vacinadas.
-      </b-card-text>
-      <template #footer>
-        <small class="text-muted">Publicado a 3 minutos atrás</small>
-      </template>
-    </b-card>
-  </b-card-group>
+    <div class="card">
+  <div class="card-body">
+    This is some text within a card body.
+  </div>
+</div>
     <Rodape/>
   </div>
 </template>
