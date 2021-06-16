2967992a7430adea1b37cee7982442f743db5428
sauloborghi <30803762+sauloborghi@users.noreply.github.com>
2021-06-16 19:28:35 -0300

    Add files via upload

---
+++ src/components/Card.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <br>
+    <h3 class="center">CENTROS ESPÍRITAS</h3>
+    <h1 class="center">MAIS PRÓXIMOS</h1>
+    <hr class="hr">
+  <b-card-group deck>
+    <b-card title="Taquaritinga já tem mais de 10% da população vacinada contra COVID-19" img-src="https://sauloborghi.github.io/ProjetoIII/assets/dist/img/roundel_stat3.png" img-alt="Image" img-top>
+      <b-card-text>
+        Mesmo com a grande dificuldade na obtenção de vacinas contra a COVID-19 neste momento que estamos vivendo, com o início da vacinação de idosos com 70 anos ou mais nesta terça-feira (23), Taquaritinga alcançou a marca de 10,17% de pessoas vacinadas.
+      </b-card-text>
+      <template #footer>
+        <small class="text-muted">Publicado a 3 minutos atrás</small>
+      </template>
+    </b-card>
+
+    <b-card title="Vacinação do povo de Taquaritinga" img-src="https://www.socialismocriativo.com.br/wp-content/uploads/2021/01/coronavac-1.jpg" img-alt="Image" img-top>
+      <b-card-text>
+        A vacinação, decorrente da ordem cronológica estabelecida pelo Estado de São Paulo (SP) chegou hoje, no dia 3/25/2021. A idade requisitada para hoje era de 69 nove anos, descendo cada vez mais para chegar até a população mais jovem.
+      </b-card-text>
+      <template #footer>
+        <small class="text-muted">Publicado a 16 horas atrás</small>
+      </template>
+    </b-card>
+
+    <b-card title="Centro Espírita Araraquara" img-src="https://www.socialismocriativo.com.br/wp-content/uploads/2021/01/coronavac-1.jpg" img-alt="Image" img-top>
+      <b-card-text>
+        Mesmo com a grande dificuldade na obtenção de vacinas contra a COVID-19 neste momento que estamos vivendo, com o início da vacinação de idosos com 50 anos ou mais nesta quarta-feira (16), Taquaritinga alcançou a marca de 25,17% de pessoas vacinadas.
+      </b-card-text>
+      <template #footer>
+        <small class="text-muted">Publicado a 23 horas atrás</small>
+      </template>
+    </b-card>
+  </b-card-group>
+</div>
+</template>
+
+<style scoped>
+  .center{
+    text-align: center;
+  }
+  .hr{
+    margin-left: 15px;
+    margin-right: 15px;
+  }
+  </style>
+
+<style scoped>
+.card-deck{
+    padding: 0px;
+    margin-top: 10px;
+    margin-right: 0;
+    margin-left: 0;
+    margin-bottom: 10px;
+}  
+</style>
+
+<script>
+export default {
+  name: 'Card',
+}
+</script>
---
+++ src/components/Carousel.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="4000"
+      controls
+      indicators
+      background="#ababab"
+      img-width="1024px"
+      img-height="48px"
+      style="text-shadow: 1px 1px 2px #333;"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <!-- Text slides with image -->
+      <b-carousel-slide
+        caption="Dia Mundial da Saúde 2021: Saúde para todos"
+        text="O Dia Mundial da Saúde é comemorado no dia 7 de abril. A data foi criada em 1950 e é O objetivo da data é garantir o melhor nível de saúde para as pessoas em todo o mundo."
+        img-src="https://sauloborghi.github.io/ProjetoIII/assets/dist/img/Banner/Banner1.jpeg"
+      ></b-carousel-slide>
+
+      <!-- Slides with custom text -->
+      <b-carousel-slide
+      caption="Taquaritinga começa a vacinação do COVID-19"
+      text="Taquaritinga começa a vacinação do COVID-19 nesse mês de março em 2021"
+      img-src="https://sauloborghi.github.io/ProjetoIII/assets/dist/img/Banner/Banner2.jpeg">
+      </b-carousel-slide>
+
+      <!-- Slides with image only -->
+      <b-carousel-slide
+      caption="Butantan"
+      text="Butantan entrega mais 2,2 milhões de doses de vacina CoronaVac"
+      img-src="https://sauloborghi.github.io/ProjetoIII/assets/dist/img/Banner/Banner3.jpeg"></b-carousel-slide>
+    
+    </b-carousel>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        slide: 0,
+        sliding: null
+      }
+    }
+  }
+</script>
+
+<script>
+export default {
+  name: 'Carousel',
+}
+</script>
